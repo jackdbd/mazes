aa33f8b3650c0927003b99570d89d01e29d85bba
Giacomo Debidda <giacomo@giacomodebidda.com>
2024-09-21 00:29:39 +0200

    add Babashka tasks to manage the project

---
+++ bb/tasks.clj
@@ -0,0 +1,16 @@
+(ns tasks
+  (:require [babashka.classpath :refer [get-classpath split-classpath]]))
+
+(defn print-classpath
+  []
+  (println "=== CLASSPATH BEGIN ===")
+  ;; all paths on a single line
+  ;; (prn (split-classpath (get-classpath)))
+  ;; each path on its own line
+  (doseq [path (split-classpath (get-classpath))]
+    (println path))
+  (println "=== CLASSPATH END ==="))
+
+(comment
+  (print-classpath)
+  )
